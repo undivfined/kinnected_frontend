eb33568e9aab9aea56235dd1b7073a69b2529b16
CCronje96 <chantellec.github@gmail.com>
2025-04-11 17:25:15 +0100

    fixed import error in TabsNavigator.tsx

---
+++ src/components/DeleteConnection.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function DeleteConnection() {
+    return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <Text>Delete Connection Button</Text>
+        </View>
+      );
+}
--- src/navigation/TabsNavigator.tsx
+++ src/navigation/TabsNavigator.tsx
@@ -1,5 +1,7 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import AddConnection from '../components/AddConnection';
+import DeleteConnection from '../components/DeleteConnection';
 
 export type RootTabParamList = {
     AddConnection: undefined;
@@ -11,7 +13,7 @@ const Tab = createBottomTabNavigator<RootTabParamList>();
 export default function TabNavigator() {
   return (
     <NavigationContainer>
-      <Tab.Navigator initialRouteName="Home">
+      <Tab.Navigator>
         <Tab.Screen name="AddConnection" component={AddConnection}/>
                 {/* note: component, as a prop, refers to which screen the stack navigator points to, NOT UI components*/}
         <Tab.Screen name="DeleteConnection" component={DeleteConnection} />
