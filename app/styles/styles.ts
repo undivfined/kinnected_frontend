a6eab7d3b7221c7ba9c9f3b45cbd19b0cd5d3922
cntencra <cn10cra@gmail.com>
2025-04-15 21:55:41 +0100

    fix: moved the nativewind styles to a styles file

---
+++ app/styles/styles.ts
@@ -0,0 +1,21 @@
+export const container = "flex-1 justify-center items-center m-10"
+
+export const headingOne = "mb-10 text-4xl font-bold"
+
+export const enterBorder = "rounded-full p-4 border border-gray-400 border-2"
+
+export const strapLine = "mt-10"
+
+export const enterPressable = "w-40 h-40 bg-gray-400 rounded-full items-center justify-center"
+
+export const headingTwo = "mb-10 text-3xl font-bold"
+
+export const logInLabel = "text-m font-bold mb-3"
+
+export const inputLabel = "text-m font-bold mb-3 text-left w-[300px]"
+
+export const textInput = "border border-black rounded-md w-[300px] h-[45px] pt-[10px] pl-[10px] text-center mb-5"
+
+export const pickerInput = "border border-black rounded-md w-[300px] h-[45px] pb-[40px] pt-0 text-center mb-5 text-black"
+
+export const logIn = "rounded-md w-3/5 px-4 py-2 text-center mb-10 mt-10 bg-black items-center "
