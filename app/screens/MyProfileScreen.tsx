7c07406eccbaff59534d897b28ffdd78b728d652
cntencra <cn10cra@gmail.com>
2025-04-15 21:59:35 +0100

    fix: deleted unused screens

--- app/screens/MyProfileScreen.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function MyProfileScreen() {
-  return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>My Profile Screen</Text>
-    </View>
-  );
-}
