0b0836486998e99fa87b25a99a8b3da9091c3c9c
ConCat99 <concatgithub@gmail.com>
2025-04-16 14:04:40 +0100

    added ConnectedAfterSignUp screen

--- app/navigation/StackNavigator.tsx
+++ app/navigation/StackNavigator.tsx
@@ -3,27 +3,33 @@ import LandingScreen from '../screens/LandingScreen';
 import LogInScreen from '../screens/LogInScreen';
 import SignUpScreen from '../screens/SignUpScreen';
 import TabNavigator from './TabNavigator';
+import ConnectAfterSignUp from '../screens/ConnectAfterSignUp';
 
 export type RootStackParamList = {
-  LandingScreen: undefined;
-  LogInScreen: undefined;
-  Home: undefined;
-  // undefined means this screen takes no parameters when being navigated to
-  // {} key-value means screen can take parameters of a specified type
-  SignUpScreen: undefined;
-  TabNavigator: undefined;
+	LandingScreen: undefined;
+	LogInScreen: undefined;
+	Home: undefined;
+	// undefined means this screen takes no parameters when being navigated to
+	// {} key-value means screen can take parameters of a specified type
+	SignUpScreen: undefined;
+	ConnectAfterSignUp: undefined;
+	TabNavigator: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function StackNavigator() {
-  return (
-      <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="LandingScreen">
-        <Stack.Screen name="LandingScreen" component={LandingScreen} />
-        <Stack.Screen name="LogInScreen" component={LogInScreen} />
-        {/* note: component, as a prop, refers to which screen the stack navigator points to, NOT UI components*/}
-        <Stack.Screen name="SignUpScreen" component={SignUpScreen} />
-        <Stack.Screen name="TabNavigator" component={TabNavigator} />
-      </Stack.Navigator>
-  );
+	return (
+		<Stack.Navigator
+			screenOptions={{ headerShown: false }}
+			initialRouteName='LandingScreen'
+		>
+			<Stack.Screen name='LandingScreen' component={LandingScreen} />
+			<Stack.Screen name='LogInScreen' component={LogInScreen} />
+			{/* note: component, as a prop, refers to which screen the stack navigator points to, NOT UI components*/}
+			<Stack.Screen name='SignUpScreen' component={SignUpScreen} />
+			<Stack.Screen name='ConnectAfterSignUp' component={ConnectAfterSignUp} />
+			<Stack.Screen name='TabNavigator' component={TabNavigator} />
+		</Stack.Navigator>
+	);
 }
