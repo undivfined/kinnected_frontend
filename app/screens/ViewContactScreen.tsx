d932fedb6ac16cc9bd7575a292b26dcb348985c8
CCronje96 <chantellec.github@gmail.com>
2025-04-17 14:32:13 +0100

    Resolved Conflicts

--- app/screens/ViewContactScreen.tsx
+++ app/screens/ViewContactScreen.tsx
@@ -1,11 +1,10 @@
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { RootStackParamList } from "../navigation/StackNavigator";
-import { container, lastContactedTile, profileImage } from "../styles/styles";
-import { ScrollView, Text, View } from "react-native";
-import { useEffect, useState } from "react";
+import { View } from "react-native";
+import { container, profileImage } from "../styles/styles";
+import { useState } from "react";
 import ImageViewer from "../components/ImageViewer";
-
-
+import { ScrollView } from "react-native-reanimated/lib/typescript/Animated";
 
 const blankProfileImg = require('../../assets/freepik-basic-placeholder-profile-picture.png')
 
@@ -29,16 +28,6 @@ export default function ViewContactScreen({ navigation }: Props){
             <View className={container}>
                 <View className={profileImage}>
                     <ImageViewer imgSource={blankProfileImg} selectedImage={selectedImage} className={profileImage}/>
-                </View>
-
-                <View className={lastContactedTile}>
-
-                </View>
-
-                <View className={lastContactedTile}>
-                    <Text className="absolute top-2 left-2 text-sm font-semibold text-gray-700">Last Contacted:
-                    </Text>
-                    <Text className="absolute bottom-2 right-2 text-xs text-gray-500">3 days ago</Text>
                 </View>
 
 
