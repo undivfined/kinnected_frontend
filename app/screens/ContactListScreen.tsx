00c3e7b496fa4f6e33b550fcadd4a5e3340cab6b
marmaryia <marozavamaryia@gmail.com>
2025-04-21 15:36:56 +0100

    show list conditional on the user in the context

--- app/screens/ContactListScreen.tsx
+++ app/screens/ContactListScreen.tsx
@@ -1,75 +1,78 @@
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { RootStackParamList } from '../navigation/StackNavigator';
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../navigation/StackNavigator";
 import {
-	ScrollView,
-	Text,
-	View,
-	TextInput,
-	FlatList,
-	Pressable,
-} from 'react-native';
+  ScrollView,
+  Text,
+  View,
+  TextInput,
+  FlatList,
+  Pressable,
+} from "react-native";
 import {
-	contactsContainer,
-	container,
-	headingFive,
-	headingThree,
-	inputLabel,
-	profileImage,
-	textInput,
-	userContainer,
-} from '../styles/styles';
-import { Profiler, useContext, useEffect, useState } from 'react';
+  contactsContainer,
+  container,
+  headingFive,
+  headingThree,
+  inputLabel,
+  profileImage,
+  textInput,
+  userContainer,
+} from "../styles/styles";
+import { Profiler, useContext, useEffect, useState } from "react";
 
-import ImageViewer from '../components/ImageViewer';
-import ContactTile from '../components/ContactTile';
-import { getContacts } from '../../api';
-import { contact } from '../../types/databaseTypes';
+import ImageViewer from "../components/ImageViewer";
+import ContactTile from "../components/ContactTile";
+import { getContacts } from "../../api";
+import { contact } from "../../types/databaseTypes";
+import { UserContext } from "../context/UserContext";
 
-const blankProfileImg = require('../../assets/freepik-basic-placeholder-profile-picture.png');
+const blankProfileImg = require("../../assets/freepik-basic-placeholder-profile-picture.png");
 
-type Props = NativeStackScreenProps<RootStackParamList, 'ContactListScreen'>;
+type Props = NativeStackScreenProps<RootStackParamList, "ContactListScreen">;
 
 export default function ContactListScreen({ navigation }: Props) {
-	const [userName, setUserName] = useState('barbara38');
-	const [selectedImage, setSelectedImage] = useState<'string' | undefined>(
-		undefined
-	);
-	const [contacts, setContacts] = useState([]);
+  const { userDetails } = useContext(UserContext);
+  const [selectedImage, setSelectedImage] = useState<"string" | undefined>(
+    undefined
+  );
+  const [contacts, setContacts] = useState([]);
 
-	useEffect(() => {
-		getContacts(userName).then((newContacts) => {
-			setContacts(newContacts);
-		});
-	}, []);
+  useEffect(() => {
+    if (userDetails.username) {
+      getContacts(userDetails.username).then((newContacts) => {
+        setContacts(newContacts);
+      });
+    }
+  }, [userDetails]);
 
-	return (
-		<View className={contactsContainer}>
-			<View className={profileImage}>
-				<ImageViewer
-					imgSource={blankProfileImg}
-					selectedImage={selectedImage}
-					className={profileImage}
-				/>
-			</View>
+  return (
+    <View className={contactsContainer}>
+      <View className={profileImage}>
+        <ImageViewer
+          imgSource={blankProfileImg}
+          selectedImage={selectedImage}
+          className={profileImage}
+        />
+      </View>
 
-			<Text className={headingFive}>{`${userName}`}</Text>
-			<Text className={headingFive}>Your Kinnections List</Text>
+      <Text className={headingFive}>{userDetails.username}</Text>
+      <Text className={headingFive}>Your Kinnections List</Text>
 
-			<Pressable
-				onPress={() => {
-					navigation.navigate('SearchedProfileScreen');
-				}}
-			>
-				<View>
-					<Text className='underline'>SearchForUsers</Text>
-				</View>
-			</Pressable>
+      <Pressable
+        onPress={() => {
+          navigation.navigate("SearchedProfileScreen");
+        }}
+      >
+        <View>
+          <Text className="underline">SearchForUsers</Text>
+        </View>
+      </Pressable>
 
-			<FlatList
-				data={contacts}
-				renderItem={({ item }) => <ContactTile contact={item} />}
-				keyExtractor={(item: contact) => item.contact_id}
-			/>
-		</View>
-	);
+      <FlatList
+        data={contacts}
+        renderItem={({ item }) => <ContactTile contact={item} />}
+        keyExtractor={(item: contact) => item.contact_id}
+      />
+    </View>
+  );
 }
