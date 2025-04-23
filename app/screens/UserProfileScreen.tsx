dcfe0517082389ed995549413365870e7234e0ed
JeremyOnyango <jezonyango@live.com>
2025-04-23 10:04:12 +0100

    Updated User Profile + extras

--- app/screens/UserProfileScreen.tsx
+++ app/screens/UserProfileScreen.tsx
@@ -1,129 +1,154 @@
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { RootStackParamList } from '../navigation/StackNavigator';
 import { useContext, useState } from 'react';
-import { Picker } from '@react-native-picker/picker';
-import DateTimePicker, {
-	DateTimePickerEvent,
-} from '@react-native-community/datetimepicker';
+import { Picker } from "@react-native-picker/picker"
+import DateTimePicker,{ DateTimePickerEvent } from '@react-native-community/datetimepicker';
 
 import ImageViewer from '../components/ImageViewer';
-import pickImage from '../utils/pickImage';
+import pickImage from "../utils/pickImage"
 
-import {
-	View,
-	Pressable,
-	Text,
-	Button,
-	TextInput,
-	ScrollView,
-} from 'react-native';
+import { View, Pressable, Text, Button, TextInput, ScrollView} from 'react-native';
 import { styles } from '../styles/styles';
 
 import { ImageContext } from '../context/ImageContext';
-import { UserContext } from '../context/UserContext';
+import { UserContext } from "../context/UserContext"
 
-const PlaceholderImage = require('../../assets/freepik-basic-placeholder-profile-picture.png');
+const PlaceholderImage = require("../../assets/freepik-basic-placeholder-profile-picture.png");
+
 
 type Props = NativeStackScreenProps<RootStackParamList, 'UserProfileScreen'>;
 
-export default function UserProfileScreen({ navigation }: Props) {
-	const { selectedImage, setSelectedImage } = useContext(ImageContext);
+export default function UserProfileScreen({ navigation } : Props) {
 
-	// const { userDetails, setUserDetails } = useContext(UserContext)
+const { selectedImage, setSelectedImage } = useContext(ImageContext)
+const { userDetails, setUserDetails } = useContext(UserContext)
 
-	const [date, setDate] = useState(new Date('1992-5-5'));
-	const [showCalender, setShowCalender] = useState(false);
-	const [country, setCountry] = useState<string | null>(null);
-	const [timezone, setTimezone] = useState<string | null>(null);
+const [isEditing, setIsEditing] = useState(false);
+const [username, setUsername] = useState(userDetails.username);
+const [lastName, setLastName] = useState(userDetails.last_name);
+const [firstName, setFirstName] = useState(userDetails.first_name);
+const [date, setDate] = useState<Date>(userDetails.date_of_birth? new Date(userDetails.date_of_birth) : new Date());
+const [showCalender, setShowCalender] = useState(false);
+const [country, setCountry] = useState(userDetails.country);
+const [timezone, setTimezone] = useState(userDetails.timezone);
 
-	function onDateChange(event: DateTimePickerEvent, selectedDate?: Date) {
-		if (event.type === 'set' && selectedDate) {
-			setDate(selectedDate);
-		}
-		setShowCalender(false);
-	}
 
-	return (
-		<ScrollView contentContainerClassName={styles.scrollContainer}>
-			<View>
-				<Text className={styles.headingTwo}>My Profile</Text>
-			</View>
+  
+    function onDateChange(event: DateTimePickerEvent, selectedDate?: Date) {
+      if (event.type === 'set' && selectedDate) {
+        setDate(selectedDate);
+      }
+      setShowCalender(false);
+    }
 
-			<View className={styles.imageContainer}>
-				<ImageViewer
-					imgSource={PlaceholderImage}
-					selectedImage={selectedImage}
-				/>
-			</View>
+  
+    const handleSave = () => {
+      setUserDetails({
+        ...userDetails,
+        username,
+        first_name: firstName,
+        last_name: lastName,
+        date_of_birth: date,
+        country,
+        timezone,
+      });
+      setIsEditing(false);
+    };
 
-			<View className={styles.logIn}>
-				<Button
-					title='Choose a photo'
-					color='black'
-					onPress={() => pickImage(setSelectedImage)}
-				/>
-			</View>
+    return (
+        <ScrollView>
 
-			<Text className={styles.inputLabel}>Username</Text>
-			<TextInput className={styles.textInput} />
+        <View className={styles.container}>
 
-			<Text className={styles.inputLabel}>Full Name</Text>
-			<TextInput className={styles.textInput} />
+          <Text className={styles.headingTwo}>My Profile</Text>
+          
 
-			<Text className={styles.inputLabel}>Date of Birth</Text>
-			<Pressable
-				className={styles.textInput}
-				onPress={() => {
-					setShowCalender(true);
-				}}
-			>
-				<Text>{date.toLocaleDateString()}</Text>
-			</Pressable>
+          <View>
+            <ImageViewer imgSource={PlaceholderImage} selectedImage={selectedImage} className={styles.profileImage}/>
+          </View>
 
-			{showCalender && (
-				<DateTimePicker
-					value={date}
-					mode='date'
-					onChange={(event, selectedDate) => {
-						onDateChange(event, selectedDate);
-					}}
-				/>
-			)}
+            <View className={styles.pictureButton}>
+              <Button title="Choose a photo" color="black" onPress={() => pickImage(setSelectedImage)}/>
+            </View>
+            
 
-			<Text className={styles.inputLabel}>Country</Text>
-			<View className={styles.pickerInput}>
-				<Picker
-					selectedValue={country}
-					onValueChange={(selected) => setCountry(selected)}
-					mode='dropdown'
-				>
-					<Picker.Item
-						label='Select your country'
-						value={null}
-						enabled={false}
-					/>
-					<Picker.Item label='England' value='england' />
-					<Picker.Item label='Belarus' value='belarus' />
-					<Picker.Item label='South Africa' value='south africa' />
-					<Picker.Item label='London' value='london' />
-				</Picker>
-			</View>
+            <Text className={styles.headingThree}>My Details</Text>
 
-			<Text className={styles.inputLabel}>Timezone</Text>
-			<View className={styles.pickerInput}>
-				<Picker
-					selectedValue={timezone}
-					onValueChange={(selected) => setTimezone(selected)}
-				>
-					<Picker.Item
-						label='Select your Timezone'
-						value={null}
-						enabled={false}
-					/>
-					<Picker.Item label='No idea' value='No idea' />
-					<Picker.Item label='hmmm' value='hmm' />
-				</Picker>
-			</View>
-		</ScrollView>
-	);
+              <Pressable className={styles.logIn} onPress={() => {
+                if (isEditing) {
+                  handleSave();
+                } else {
+                  setIsEditing(true);
+                }
+              }}>
+                <Text className="text-white">{isEditing ? 'Save' : 'Edit'}</Text>
+              </Pressable>
+
+
+            <Text className={styles.inputLabel}>Username</Text>
+              <TextInput
+                className={styles.textInput}
+                value={username}
+                onChangeText={setUsername}
+                editable={isEditing}
+              />
+
+
+            <Text className={styles.inputLabel}>First Name</Text>
+              <TextInput
+                className={styles.textInput}
+                value={firstName}
+                onChangeText={setFirstName}
+                editable={isEditing}
+              />
+
+
+            <Text className={styles.inputLabel}>Last Name</Text>
+              <TextInput
+                className={styles.textInput}
+                value={lastName}
+                onChangeText={setLastName}
+                editable={isEditing}
+              />
+
+
+            <Text className={styles.inputLabel}>Date of Birth</Text>
+            <Pressable className={styles.textInput} onPress={()=>{ isEditing && setShowCalender(true)}}><Text>{date.toLocaleDateString()}</Text></Pressable>
+
+            {showCalender && (
+            <DateTimePicker value={date || new Date} mode="date" onChange={(event, selectedDate) => {onDateChange(event, selectedDate)}}/>
+            )}
+            
+
+            <Text className={styles.inputLabel}>Country</Text>
+            <View className={styles.pickerInput} >
+              <Picker selectedValue={country}  onValueChange={(selected) => setCountry(selected)} mode="dropdown" enabled={isEditing}>
+                <Picker.Item label="Select your country" value={null} enabled={false} />
+                <Picker.Item label="England" value="england" />
+                <Picker.Item label="Belarus" value="belarus" />
+                <Picker.Item label="South Africa" value="south africa" />
+                <Picker.Item label="London" value="london" />
+              </Picker>
+            </View>
+            
+             
+            <Text className={styles.inputLabel}>Timezone</Text>
+            <View className={styles.pickerInput} >
+              <Picker selectedValue={timezone} onValueChange={(selected) => setTimezone(selected)} mode="dropdown" enabled={isEditing}>
+                <Picker.Item label="Select your Timezone" value={null} enabled={false} />
+                <Picker.Item label="No idea" value="No idea" />
+                <Picker.Item label="hmmm" value="hmm" />
+              </Picker>
+            </View>
+
+
+            <Pressable className={styles.deleteButton} onPress={() => {navigation.navigate("LandingScreen")
+            }}>
+              <Text className="text-white">Delete Account</Text>
+            </Pressable>
+
+          </View>
+
+        </ScrollView>
+      );
 }
--- app/utils/pickImage.ts
+++ app/utils/pickImage.ts
@@ -1,17 +1,25 @@
-
 import * as ImagePicker from "expo-image-picker"
+import {ImageContextType} from "../context/ImageContext"
+import { Platform } from "react-native";
 
-export default function pickImage(setSelectedImage: Function) {
+export default function pickImage( setSelectedImage: ImageContextType['setSelectedImage']) {
+
     ImagePicker.launchImageLibraryAsync({
       mediaTypes: ['images'],
       allowsEditing: true,
       quality: 1,
     })
     .then((result) => {
-    if (!result.canceled) {
-      setSelectedImage(result.assets[0].uri);    
-    } else {
-      alert('You did not select any image.');
+      if (!result.canceled) {
+        const uri = result.assets[0].uri;
+        const base64 = result.assets[0].base64;
+    
+        // For web, use base64; for mobile, use URI
+        const imageSource = Platform.OS === 'web' ? `data:image/jpeg;base64,${base64}` : uri;
+        setSelectedImage(imageSource);
+      } else {
+        alert('You did not select a new image.');
+      }
     }
-  });
-}
+)}
+ 
