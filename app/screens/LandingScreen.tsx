a82c75bf797df225e758108b0b644ad491b09769
Shantelle Awomoyi <shantelleawo@gmail.com>
2025-04-24 11:14:03 +0100

    edited landing page

--- app/screens/LandingScreen.tsx
+++ app/screens/LandingScreen.tsx
@@ -2,24 +2,29 @@ import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { View, Pressable, Text } from 'react-native';
 import { RootStackParamList } from '../navigation/StackNavigator';
 import { styles } from '../styles/styles';
+import ImageViewer from '../components/ImageViewer';
+
+const kinnectedLogo =  require('../../assets/Kinnected_logo.png')
+
 
 type Props = NativeStackScreenProps<RootStackParamList, 'LandingScreen'>;
 
 export default function LandingScreen({ navigation }: Props) {
 	return (
 		<View className={styles.container}>
-			<Text className={styles.headingOne}>Kinnected</Text>
-			<View className={styles.enterBorder}>
-				<Pressable
+				<Pressable 
+			
 					className={styles.enterPressable}
 					onPress={() => {
 						navigation.navigate('LogInScreen');
 					}}
 				>
-					<Text className={styles.submitButtonText}>Enter</Text>
+					
+					<ImageViewer className={styles.enterPressable} imgSource={kinnectedLogo} />
+					{/* <Text className={styles.headingFour}> Get Kinnected</Text> */}
+					<Text className={styles.strapLine}>Where Your Connections Become Kin</Text>
 				</Pressable>
-			</View>
-			<Text className={styles.strapLine}>I am an app</Text>
+	
 		</View>
 	);
 }
