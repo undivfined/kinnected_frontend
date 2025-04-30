ee87ae614ebf4b59fac5594ca2c2ef038ef26aba
marmaryia <marozavamaryia@gmail.com>
2025-04-30 18:31:56 +0100

    remove unused components

--- app/components/DeleteConnection.tsx
+++
@@ -1,9 +0,0 @@
-import { View, Text } from 'react-native';
-
-export default function DeleteConnection() {
-    return (
-        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-          <Text>Delete Connection Button</Text>
-        </View>
-      );
-}
